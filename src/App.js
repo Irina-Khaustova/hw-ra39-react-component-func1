7ed882d1540c7f70b7b7003a5e27e7a456668f28
Irina Khaustova <Irina170580@yandex.ru>
2022-06-12 22:54:22 +0300

    change item

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import ShopItemFunc from './components/ShopItemFunc';
-import Models from '../models/models.js';
+import Models from './models/models.js';
 
 
 const item = new Models ( 'Tiger of Sweden', 'Leonard coat', 'Minimalistic coat in cotton-blend', 'Men\'s minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.', 399, '£'
