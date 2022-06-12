116002941d3986bc607ccd4c0254fa97ed5e3c25
Irina Khaustova <Irina170580@yandex.ru>
2022-06-12 22:48:15 +0300

    version1

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import './App.css';
 import ShopItemFunc from './components/ShopItemFunc';
+import Models from '../models/models.js';
 
 
-const item = {
-  brand: 'Tiger of Sweden',
-  title: 'Leonard coat',
-  description: 'Minimalistic coat in cotton-blend',
-  descriptionFull: 'Men\'s minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.',
-  price: 399,
-  currency: '£'
-}
+const item = new Models ( 'Tiger of Sweden', 'Leonard coat', 'Minimalistic coat in cotton-blend', 'Men\'s minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.', 399, '£'
+)
 
 function App() {
   return (
